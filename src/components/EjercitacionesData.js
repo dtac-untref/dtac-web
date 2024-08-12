c6870a0fad2f35ef2a7ff150b2ea96f8668a339f
mavialongi <mavialongi@users.noreply.github.com>
2024-08-12 12:17:50 -0300

    agregado de ejercitacion 1

--- src/components/EjercitacionesData.js
+++ src/components/EjercitacionesData.js
@@ -1,6 +1,10 @@
 import ej_1 from "../pdfs/Ejercitacion 01-1.pdf";
 import ej_2 from "../pdfs/Ejercitacion 01-2.pdf";
 import ej_3 from "../pdfs/Ejercitacion 01-3.pdf";
+import rar_1 from "../rar/Ejercitacion-01.rar";
+
+// const archivoRAR = `${process.env.PUBLIC_URL}/public/rar/Ejercitacion-01.rar`;
+
 
 const ejercitaciones = [
   {
@@ -8,77 +12,49 @@ const ejercitaciones = [
     titulo: "Ejercitación 1",
     consigna: `
           Estimados,<br><br>
-          Esperando que se encuentren bien, comenzaremos compartiendo los Videotutoriales
-          que guiarán la cursada, les servirá para entender el sentido que le daremos al manejo
-          del programa, es decir, a la metodología de trabajo que proponemos, esta modalidad
-          de uso, será considerada a la hora de corregir. En algunas ocasiones, iremos
-          compartiendo otros videos que también muestran los temas de cada clase, que están
-          subidos a YouTube, y que no han sido generados por nosotros, pero en todos los
-          casos no dejen de ver el video específico de la materia para entender cómo
-          establecemos las pautas de uso o flujo de trabajo.<br><br>
-          Para arrancar les solicitamos que vean este tutorial sobre Introducción y
-          familiarización con el programa
-          AutoCAD: <a href="https://drive.google.com/file/d/1TasLgPJK9hKXU4yjfFIyKDtDAzpn2cQL/view?usp=sharing" target="_blank">Ver Video</a><br>
-          Aquí tienen un video de la Universidad Politécnica de Valencia sobre la apariencia de
-          del entorno Dibujo 2D y anotación, en este caso de AutoCAD 2018, en español: 
-          <a href="https://youtu.be/sIoyDF_1k-8" target="_blank">Ver Video</a><br>
-          En este video se reiteran algunas cuestiones, pero explicamos el modo de activar la
-          Barra de Menús desplegables propios del entorno Autocad Clásico dentro del entorno
-          Dibujo 2D y anotación e insisto con la manera de desactivar las ayudas de dibujo:
-          <a href="https://youtu.be/bQ5_o5XPjR0" target="_blank">Ver Video</a><br>
-          Por último, enviamos este Videotutorial sobre Ingreso de datos por coordenadas:
+          Comenzaremos compartiendo los Videos de la cursada, que determinan el sentido que le daremos al manejo del programa, es decir, a la metodolog&iacute;a de trabajo que proponemos, esta modalidad de uso ser&aacute; considerada a la hora de corregir. En algunas ocasiones, iremos compartiendo otros videos que tambi&eacute;n muestran los temas de cada clase, que est&aacute;n subidos a YouTube, y que no han sido generados por nosotros, pero en todos los casos no dejen de ver el video espec&iacute;fico de la materia para entender c&oacute;mo establecemos las pautas de uso o flujo de trabajo.&nbsp;<br><br>
+          Para arrancar les solicitamos que vean este tutorial sobre Introducci&oacute;n y familiarizaci&oacute;n con el programa AutoCAD: <a href="https://drive.google.com/file/d/1TasLgPJK9hKXU4yjfFIyKDtDAzpn2cQL/view?usp=sharing" target="_blank">Ver Video</a><br>
+          <br>
+          Aqu&iacute; tienen un video de la Universidad Polit&eacute;cnica de Valencia sobre la apariencia de del entorno<strong>&nbsp;Dibujo 2D y anotaci&oacute;n</strong>, en este caso de AutoCAD 2018, en espa&ntilde;ol:&nbsp; 
+          <a href="https://youtu.be/sIoyDF_1k-8" target="_blank">Ver Video</a>
+          <br><br>
+          En este video se reiteran algunas cuestiones, pero explicamos el modo de activar la Barra de Men&uacute;s desplegables propios del entorno Autocad Cl&aacute;sico dentro del entorno Dibujo 2D y anotaci&oacute;n e insisto con la manera de desactivar las ayudas de dibujo:
+          <a href="https://youtu.be/bQ5_o5XPjR0" target="_blank">Ver Video</a>
+          <br><br>
+          Por &uacute;ltimo, enviamos este&nbsp;Videotutorial sobre Ingreso de datos por coordenadas:
           <a href="https://drive.google.com/file/d/1ddxd_21ZiwuUU458PNpB9zuB5o-WIH-l/view?usp=sharing" target="_blank">Ver Video</a><br><br>
-          En la Sección Archivos adjuntamos el archivo Ejercitacion 01.rar, al descomprimirlo
-          encontrarán 3 ejercitaciones, para que las ejecuten y puedan practicar la generación
-          de vectores por medio del ingreso de datos por coordenadas utilizando, el comando
-          línea, el comando borrar, el zoom en tiempo real con la ruedita del mouse y el paneo,
-          panorámico o encuadre (Presionando la ruedita del mouse). Para trabajar en esta
-          instancia donde nos encontramos, es fundamental que recuerden desactivar todas las
-          ayudas de dibujo ubicadas en la barra de estado, debajo de la línea de comandos.
-          ESPECIALMENTE LES SOLICITAMOS QUE DESACTIVEN ENTRADA DINAMICA (F12).<br><br>
-          Los archivos adjuntos se componen por tres figuras, la primera (Ejercitacion 01- 1.pdf) es la que se explica en el video, las otras dos (Ejercitacion 01- 2.pdf y Ejercitacion 01-
-3.pdf) son de similares características, la última se puede comenzar a dibujar en
-cualquier parte de nuestro espacio de trabajo, pues no tiene especificado una coordenada absoluta de arranque. NO se debe acotar, las cotas y la anotación de los
-distintos puntos son solo para poder ejecutar las figuras. Por cada actividad deben
-abrir un archivo o plantilla nueva de dibujo. Para esta primera práctica es obligatorio la
-entrega de las 3 ejercitaciones.
-En este video reitero el tema de las plantillas o template (drawing1.dwg o
-dibujo1.dwg), además de insistir sobre el posicionamiento, con el comando zoom y
-encuadre que nos permite estar ubicados dentro del espacio de
-trabajo: <a href="https://youtu.be/GVo94OtE2_s" target="_blank">Ver Video</a><br><br>
-Repetimos que la realización de las ejercitaciones es requisito para la aprobación de
-la cursada, y deben ser entregadas en tiempo y forma a la casilla:
-dtacun3f@gmail.com. Recuerden que las actividades son individuales y que existe la
-manera de corroborar la computadora donde han sido generados los archivos.
-No olviden salvar los archivos para versión 2013
-(<a href="https://drive.google.com/file/d/19tifCLbG-fIVA6BMZsos1IZmk-ayZiv6/view?
-usp=sharing" target="_blank">Ver Video</a>) y denominarlos adecuadamente:
-Ejercitacion 01-1 Apellido.dwg.
-Ejercitacion 01-2 Apellido.dwg.
-Ejercitacion 01-3 Apellido.dwg. <br><br>
-La fecha límite de entrega queda pautada para luego del próximo encuentro, para plantear las dudas y luego de
-la clase enviar los archivos, por si deben corregir algo. Así no atestamos la casilla de correo con dos versiones de la misma actividad, porque eso puede generar confusión.
-Consideramos que es altamente beneficioso que participen y se expresen en el foro o en el grupo de WhatsApp, formulando sus impresiones, dificultades o alternativas encontradas para poder llevar adelante las ejercitaciones, es interesante que puedan
-formar contactos y vínculos entre ustedes para ayudarse y no sentirse solos frente a
-la máquina mientras estudian. Creemos que interviniendo en este espacio,
-manifestando las consultas o las dudas para que los propios compañeros puedan
-ayudarnos, es un excelente hábito de socialización y de formar comunidad para
-acompañarnos entre todos, si alguien sabe de un tema o puede compartir cómo lo
-hizo, no solo está ayudando a uno o más compañeros, sino que también nos está
-dando una mano, ya que esa respuesta o sugerencia entre compañeros tal vez llega
-antes de que nosotros podamos verla y dedicarnos a responderla o tal vez aporta otro
-punto de vista u otra manera de resolver la situación.<br><br>
-Les mandamos saludos y seguimos en contacto.
-      `,
+          Adjuntamos los 3 archivos de las ejercitaciones, para que las ejecuten y puedan practicar la generaci&oacute;n de vectores por medio del ingreso de datos por coordenadas utilizando, el comando l&iacute;nea, el comando borrar, el zoom en tiempo real con la ruedita del mouse y el paneo, panor&aacute;mico o encuadre (presionando la ruedita del mouse). Para trabajar en esta instancia donde nos encontramos, es fundamental que recuerden <strong>desactivar todas las ayudas de dibujo ubicadas en la barra de estado</strong>, debajo de la l&iacute;nea de comandos. ESPECIALMENTE LES SOLICITAMOS QUE DESACTIVEN <strong>ENTRADA DINAMICA</strong> (F12). Los archivos adjuntos se componen por tres figuras, la primera (<strong><em>Ejercitacion 01- 1.pdf</em></strong>) es la que se explica en el video, las otras dos (<strong><em>Ejercitacion 01- 2.pdf&nbsp;</em></strong>y <strong><em>Ejercitacion 01- 3.pdf</em></strong>) son de similares caracter&iacute;sticas, la &uacute;ltima se puede comenzar a dibujar en cualquier parte de nuestro espacio de trabajo, pues no tiene especificado una coordenada absoluta de arranque. NO se debe acotar, las cotas y la anotaci&oacute;n de los distintos puntos son solo para poder ejecutar las figuras. Por cada actividad deben abrir un archivo o plantilla nueva de dibujo. Para esta primera pr&aacute;ctica es obligatorio la entrega de las 3 ejercitaciones.
+          <br><br>
+          En este video reitero el tema de las <em>plantillas</em> o <em>template</em> (drawing1.dwg o dibujo1.dwg), adem&aacute;s de insistir sobre el posicionamiento, con el comando zoom y encuadre que nos permite estar ubicados dentro del espacio de trabajo: <a href="https://youtu.be/GVo94OtE2_s" target="_blank">Ver Video</a>
+          <br><br>
+          <strong>Repetimos que&nbsp;la realizaci&oacute;n de las ejercitaciones es requisito para la aprobaci&oacute;n de la cursada, y deben ser entregadas en tiempo y forma a la casilla:</strong> <strong>dtacun3f@gmail.com</strong><strong>. Recuerden que las actividades son individuales y que existe la manera de corroborar la computadora donde han sido generados los archivos.</strong>
+          <br><br>          
+          No olviden salvar los archivos para versi&oacute;n 2013
+          (<a href="https://drive.google.com/file/d/19tifCLbG-fIVA6BMZsos1IZmk-ayZiv6/view?
+          usp=sharing" target="_blank">Ver Video</a>) y denominarlos adecuadamente:
+          <br><br>
+          <strong><em>Ejercitacion 01-1Apellido.dwg</em></strong>
+          <br><br>
+          <strong><em>Ejercitacion 01-2 Apellido.dwg</em></strong>
+          <br><br>
+          <strong><em>Ejercitacion 01-3 Apellido.dwg</em></strong>
+          <br><br>
+          La fecha l&iacute;mite de entrega queda pautada para luego del pr&oacute;ximo encuentro, considerando el plazo m&aacute;ximo para el <strong>Jueves 15/8 (comision Miercoles)</strong> y <strong>Viernes 16/8 (comisi&oacute;n Jueves)</strong>, para exponer el trabajo, plantear las dudas y luego de la clase enviar los archivos, por si deben corregir algo. As&iacute; no atestamos la casilla de correo con dos versiones de la misma actividad, porque eso puede generar confusi&oacute;n.&nbsp;<br><br>          
+          Consideramos que es altamente beneficioso que participen y se expresen en el foro o en el grupo de WhatsApp, formulando sus impresiones, dificultades o alternativas encontradas para poder llevar adelante las ejercitaciones, es interesante que puedan formar contactos y v&iacute;nculos entre ustedes para ayudarse y no sentirse solos frente a la m&aacute;quina mientras estudian. Creemos que interviniendo en este espacio, manifestando las consultas o las dudas para que los propios compa&ntilde;eros puedan ayudarnos, es un excelente h&aacute;bito de socializaci&oacute;n y de formar comunidad para acompa&ntilde;arnos entre todos, si alguien sabe de un tema o puede compartir c&oacute;mo lo hizo, no solo est&aacute; ayudando a uno o m&aacute;s compa&ntilde;eros, sino que tambi&eacute;n nos est&aacute; dando una mano, ya que esa respuesta o sugerencia entre compa&ntilde;eros tal vez llega antes de que nosotros podamos verla y dedicarnos a responderla o tal vez aporta otro punto de vista u otra manera de resolver la situaci&oacute;n.<br><br>
+          Les mandamos saludos y seguimos en contacto.<br><br>`,
     videos: [
-      "https://www.youtube.com/embed/sIoyDF_1k-8",
       "https://drive.google.com/file/d/1TasLgPJK9hKXU4yjfFIyKDtDAzpn2cQL/preview",
+      "https://www.youtube.com/embed/sIoyDF_1k-8",
+      "https://www.youtube.com/embed/bQ5_o5XPjR0",
       "https://drive.google.com/file/d/1ddxd_21ZiwuUU458PNpB9zuB5o-WIH-l/preview",
-      "https://drive.google.com/file/d/19tifCLbG-fIVA6BMZsos1IZmk-ayZiv6/view?usp=sharing",
+      "https://www.youtube.com/embed/GVo94OtE2_s",
+      "https://drive.google.com/file/d/19tifCLbG-fIVA6BMZsos1IZmk-ayZiv6/preview",
     ],
     pdfs: [ej_1, ej_2, ej_3],
     powerpoints: [],
   },
+
+  
   //   {
   //     id: 2,
   //     titulo: 'Ejercitación 2',
