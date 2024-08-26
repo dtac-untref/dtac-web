f8e3d2385a9dc32adee91be0ef0110bfb5bf7f1f
Ju4n113 <ju4n113@gmail.com>
2024-08-26 10:53:58 -0300

    Agregado actividad Cubo

--- src/components/ActividadData.js
+++ src/components/ActividadData.js
@@ -1,6 +1,7 @@
 import ej_1 from "../pdfs/Ejercitacion 01-1.pdf";
 import ej_2 from "../pdfs/Ejercitacion 01-2.pdf";
 import ej_3 from "../pdfs/Ejercitacion 01-3.pdf";
+import ej_4 from "../pdfs/Ejercitacion_04-Confeccion_de_Pieza_a_partir_del_cubo.pdf";
 // import rar_1 from "../rar/Ejercitacion-01.rar";
 
 // const archivoRAR = `${process.env.PUBLIC_URL}/public/rar/Ejercitacion-01.rar`;
@@ -35,19 +36,14 @@ const actividades = [
   },
 
 
-//   {
-//     id: 3,
-//     titulo: 'Actividad del Cubo',
-//     consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un método o elemento específico relacionado con la contruccion en seco.
-//     <br><br>
-//     El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
-// Para la clase deben traer el cubo de telgopor (Poliestireno Expandido – EPS) de 10 cm de lado, para el calado de un modelo o pieza personal a partir de la intervención. Les solicitemos que cuenten con cutter o trincheta, hojas (preferentemente cuadriculadas), alguna regla o escuadra y lápiz para trabajar en la clase.      
-//     <br><br>
-//     <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
-//     videos: [ ],
-//     pdfs: [ ],
-//     powerpoints: [ ],
-// },
+  {
+    id: 3,
+    titulo: 'Actividad del Cubo',
+    consigna: `La próxima clase nos encontraremos en el 2do piso de Caseros II, a las 14hs. Para la clase deben traer calado el modelo o pieza personal a partir de la intervención del cubo de telgopor (Poliestireno Expandido – EPS) de 10 cm de lado. Según la consigna que adjuntamos, deben realizar su propio diseño, no se debe copiar el modelo del ejemplo. Ante el caso de que les solicitemos que hagan algún ajuste, hará falta cutter o trincheta. Por último, les solicitamos que cuenten con hojas (preferentemente cuadriculadas), alguna regla o escuadra, lápiz y cinta de papel para trabajar en la clase.`,
+    videos: [ ],
+    pdfs: [ej_4],
+    powerpoints: [ ],
+},
 
 
 ];
