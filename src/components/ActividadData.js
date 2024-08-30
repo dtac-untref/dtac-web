34be6bb9ef5a8a23e7ddb277cfdc7d93b8a99798
Ju4n113 <ju4n113@gmail.com>
2024-08-30 16:10:18 -0300

    Quitamos actividad cubo.

--- src/components/ActividadData.js
+++ src/components/ActividadData.js
@@ -7,45 +7,45 @@ import ej_4 from "../pdfs/Ejercitacion_04-Confeccion_de_Pieza_a_partir_del_cubo.
 // const archivoRAR = `${process.env.PUBLIC_URL}/public/rar/Ejercitacion-01.rar`;
 
 
-const actividades = [
-  {
-    id: 1,
-    titulo: "Actividad de la Tía Elena",
-    consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un tema específico relacionado con la Ingeniería de Sonido.
-    <br><br>
-    El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
-    <br><br>
-    <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
-    videos: [ ],
-    pdfs: [ ],
-    powerpoints: [ ],
-  },
+// const actividades = [
+//   {
+//     id: 1,
+//     titulo: "Actividad de la Tía Elena",
+//     consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un tema específico relacionado con la Ingeniería de Sonido.
+//     <br><br>
+//     El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
+//     <br><br>
+//     <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
+//     videos: [ ],
+//     pdfs: [ ],
+//     powerpoints: [ ],
+//   },
 
 
-    {
-      id: 2,
-      titulo: 'Actividad de Construcción en Seco',
-      consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un método o elemento específico relacionado con la contruccion en seco.
-      <br><br>
-      El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
-      <br><br>
-      <a href="https://ju4n113.wixsite.com/website" target="_blank">Link a página de actividad</a>`,
-      videos: [ ],
-      pdfs: [ ],
-      powerpoints: [ ],
-  },
+//     {
+//       id: 2,
+//       titulo: 'Actividad de Construcción en Seco',
+//       consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un método o elemento específico relacionado con la contruccion en seco.
+//       <br><br>
+//       El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
+//       <br><br>
+//       <a href="https://ju4n113.wixsite.com/website" target="_blank">Link a página de actividad</a>`,
+//       videos: [ ],
+//       pdfs: [ ],
+//       powerpoints: [ ],
+//   },
 
 
-  {
-    id: 3,
-    titulo: 'Actividad del Cubo',
-    consigna: `La próxima clase nos encontraremos en el 2do piso de Caseros II, a las 14hs. Para la clase deben traer calado el modelo o pieza personal a partir de la intervención del cubo de telgopor (Poliestireno Expandido – EPS) de 10 cm de lado. Según la consigna que adjuntamos, deben realizar su propio diseño, no se debe copiar el modelo del ejemplo. Ante el caso de que les solicitemos que hagan algún ajuste, hará falta cutter o trincheta. Por último, les solicitamos que cuenten con hojas (preferentemente cuadriculadas), alguna regla o escuadra, lápiz y cinta de papel para trabajar en la clase.`,
-    videos: [ ],
-    pdfs: [ej_4],
-    powerpoints: [ ],
-},
+//   {
+//     id: 3,
+//     titulo: 'Actividad del Cubo',
+//     consigna: `La próxima clase nos encontraremos en el 2do piso de Caseros II, a las 14hs. Para la clase deben traer calado el modelo o pieza personal a partir de la intervención del cubo de telgopor (Poliestireno Expandido – EPS) de 10 cm de lado. Según la consigna que adjuntamos, deben realizar su propio diseño, no se debe copiar el modelo del ejemplo. Ante el caso de que les solicitemos que hagan algún ajuste, hará falta cutter o trincheta. Por último, les solicitamos que cuenten con hojas (preferentemente cuadriculadas), alguna regla o escuadra, lápiz y cinta de papel para trabajar en la clase.`,
+//     videos: [ ],
+//     pdfs: [ej_4],
+//     powerpoints: [ ],
+// },
 
 
-];
+// ];
 
 export default actividades;
