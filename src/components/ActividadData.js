d8d65d4b59b7bca4934851d061d41581da1fcbc4
unknown <alongi46403@estudiantes.untref.edu.ar>
2025-09-01 23:30:00 -0300

    actividad de cubo

--- src/components/ActividadData.js
+++ src/components/ActividadData.js
@@ -7,18 +7,18 @@ import ej_3 from "../pdfs/Ejercitacion 01-3.pdf";
 
 
 const actividades = [
-  {
-    id: 1,
-    titulo: "Actividad de la Tía Elena",
-    consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un tema específico relacionado con la Ingeniería de Sonido.
-    <br><br>
-    El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
-    <br><br>
-    <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
-    videos: [ ],
-    pdfs: [ ],
-    powerpoints: [ ],
-  },
+  // {
+  //   id: 1,
+  //   titulo: "Actividad de la Tía Elena",
+  //   consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un tema específico relacionado con la Ingeniería de Sonido.
+  //   <br><br>
+  //   El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
+  //   <br><br>
+  //   <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
+  //   videos: [ ],
+  //   pdfs: [ ],
+  //   powerpoints: [ ],
+  // },
 
 
  //   {
@@ -35,19 +35,19 @@ const actividades = [
  // },
 
 
-//   {
-//     id: 3,
-//     titulo: 'Actividad del Cubo',
-//     consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un método o elemento específico relacionado con la contruccion en seco.
-//     <br><br>
-//     El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
-// Para la clase deben traer el cubo de telgopor (Poliestireno Expandido – EPS) de 10 cm de lado, para el calado de un modelo o pieza personal a partir de la intervención. Les solicitemos que cuenten con cutter o trincheta, hojas (preferentemente cuadriculadas), alguna regla o escuadra y lápiz para trabajar en la clase.      
-//     <br><br>
-//     <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
-//     videos: [ ],
-//     pdfs: [ ],
-//     powerpoints: [ ],
-// },
+  {
+    id: 3,
+    titulo: 'Actividad del Cubo',
+    consigna: `En esta actividad grupal, tendrán la oportunidad de realizar una breve investigación y crear una presentación clara y sencilla sobre un método o elemento específico relacionado con la contruccion en seco.
+    <br><br>
+    El objetivo de esta actividad es que <strong> logren transmitir información técnica de manera sencilla y efectiva</strong>, haciendo uso de recursos visuales y una buena organización de la información.
+Para la clase deben traer el cubo de telgopor (Poliestireno Expandido – EPS) de 10 cm de lado, para el calado de un modelo o pieza personal a partir de la intervención. Les solicitemos que cuenten con cutter o trincheta, hojas (preferentemente cuadriculadas), alguna regla o escuadra y lápiz para trabajar en la clase.      
+    <br><br>
+    <a href="https://ju4n113.wixsite.com/my-site-1" target="_blank">Link a página de actividad</a>`,
+    videos: [ ],
+    pdfs: [ ],
+    powerpoints: [ ],
+},
 
 
 ];
